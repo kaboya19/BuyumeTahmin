c925931a5a5bbe2d8e02d95fb374b2f29f197ef0
Bora Kaya <kayabo19@itu.edu.tr>
2025-01-10 17:40:49 +0300

    .

--- stream.py
+++ stream.py
@@ -295,83 +295,236 @@ if page=="Çeyreklik Tahmin":
         st.plotly_chart(fig1)
 
 if page=="Yıllık Tahmin":
+    tip=st.sidebar.selectbox("Tip:", ["Mevsimsellikten Arındırılmış","Ham"])
+    if tip=="Mevsimsellikten Arındırılmış":
 
-    st.markdown(
-        """
-        <style>
-            /* Ana başlık stili */
-            .main-title {
-                font-size: 36px; /* Yazı boyutunu büyüt */
-                font-weight: bold; /* Kalın yazı */
-                color: black; /* Yazı rengi siyah */
-                text-align: center; /* Ortala */
-                margin-top: 20px; /* Üst boşluk */
-                margin-bottom: 20px; /* Alt boşluk */
-            }
-        </style>
-        <h1 class="main-title">Yıllık Büyüme Tahmini</h1>
-        """,
-        unsafe_allow_html=True
-    )
+        st.markdown(
+            """
+            <style>
+                /* Ana başlık stili */
+                .main-title {
+                    font-size: 36px; /* Yazı boyutunu büyüt */
+                    font-weight: bold; /* Kalın yazı */
+                    color: black; /* Yazı rengi siyah */
+                    text-align: center; /* Ortala */
+                    margin-top: 20px; /* Üst boşluk */
+                    margin-bottom: 20px; /* Alt boşluk */
+                }
+            </style>
+            <h1 class="main-title">Yıllık Büyüme Tahmini</h1>
+            """,
+            unsafe_allow_html=True
+        )
 
-    st.markdown(
-        """
-        <style>
-            /* Ana başlık stili */
-            .main-title {
-                font-size: 4px; /* Yazı boyutunu küçült */
-                color: black; /* Yazı rengi siyah */
-                text-align: center; /* Ortala */
-                margin-top: 5px; /* Üst boşluk */
-                margin-bottom: 5px; /* Alt boşluk */
-            }
-        </style>
-        <h3 class="main-title">*Mevsim ve takvim etkilerinden arındırılmıştır.</h3>
-        """,
-        unsafe_allow_html=True
-    )
-
-
+        st.markdown(
+            """
+            <style>
+                /* Ana başlık stili */
+                .main-title {
+                    font-size: 4px; /* Yazı boyutunu küçült */
+                    color: black; /* Yazı rengi siyah */
+                    text-align: center; /* Ortala */
+                    margin-top: 5px; /* Üst boşluk */
+                    margin-bottom: 5px; /* Alt boşluk */
+                }
+            </style>
+            <h3 class="main-title">*Mevsim ve takvim etkilerinden arındırılmıştır.</h3>
+            """,
+            unsafe_allow_html=True
+        )
 
 
-    def tarih_ceyrek(tarih):
-        yil = tarih.year
-        ay = tarih.month
-        if ay <= 3:
-            return f"{yil}Q1"
-        elif ay <= 6:
-            return f"{yil}Q2"
-        elif ay <= 9:
-            return f"{yil}Q3"
+
+
+        def tarih_ceyrek(tarih):
+            yil = tarih.year
+            ay = tarih.month
+            if ay <= 3:
+                return f"{yil}Q1"
+            elif ay <= 6:
+                return f"{yil}Q2"
+            elif ay <= 9:
+                return f"{yil}Q3"
+            else:
+                return f"{yil}Q4"
+            
+        tarihler = [tarih_ceyrek(t) for t in pd.to_datetime(yıllık.index)]
+        formatted_dates = tarihler
+
+        fig2= go.Figure()
+        if dark_mode_enabled:
+            fig2.add_trace(go.Scatter(x=yıllık.index,y=yıllık["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=4),marker=dict(size=8,color="white")))
+            fig2.add_trace(go.Scatter(x=yıllık.index[:-1],y=yıllık["Büyüme"][:-1],mode='lines+markers',name="Büyüme",line=dict(color='orange', width=4),marker=dict(size=8,color="white")))
+            fig2.update_layout(
+                xaxis=dict(
+                    tickvals=yıllık.index,  # Original datetime index
+                    ticktext=formatted_dates,  # Custom formatted labels
+                    tickfont=dict(size=14, family="Arial Black", color="white")
+                ),
+                yaxis=dict(
+                    tickfont=dict(size=14, family="Arial Black", color="white")
+                ),
+                font=dict(family="Arial", size=14, color="white")
+            )
+            
+            st.plotly_chart(fig2)
+
         else:
-            return f"{yil}Q4"
-        
-    tarihler = [tarih_ceyrek(t) for t in pd.to_datetime(yıllık.index)]
-    formatted_dates = tarihler
+            fig2.add_trace(go.Scatter(x=yıllık.index,y=yıllık["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=4),marker=dict(size=8,color="black")))
+            fig2.add_trace(go.Scatter(x=yıllık.index[:-1],y=yıllık["Büyüme"][:-1],mode='lines+markers',name="Büyüme",line=dict(color='orange', width=4),marker=dict(size=8,color="black")))
+            fig2.update_layout(
+                xaxis=dict(
+                    tickvals=yıllık.index,  # Original datetime index
+                    ticktext=formatted_dates,  # Custom formatted labels
+                    tickfont=dict(size=14, family="Arial Black", color="black")
+                ),
+                yaxis=dict(
+                    tickfont=dict(size=14, family="Arial Black", color="black")
+                ),
+                font=dict(family="Arial", size=14, color="black")
+            )
+            st.plotly_chart(fig2)
 
-    fig2= go.Figure()
-    if dark_mode_enabled:
-        fig2.add_trace(go.Scatter(x=yıllık.index,y=yıllık["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=4),marker=dict(size=8,color="white")))
-        fig2.add_trace(go.Scatter(x=yıllık.index[:-1],y=yıllık["Büyüme"][:-1],mode='lines+markers',name="Büyüme",line=dict(color='orange', width=4),marker=dict(size=8,color="white")))
-        fig2.update_layout(
-            xaxis=dict(
-                tickvals=yıllık.index,  # Original datetime index
-                ticktext=formatted_dates,  # Custom formatted labels
-                tickfont=dict(size=14, family="Arial Black", color="white")
-            ),
-            yaxis=dict(
-                tickfont=dict(size=14, family="Arial Black", color="white")
-            ),
-            font=dict(family="Arial", size=14, color="white")
+
+
+        st.markdown(
+            """
+            <style>
+                /* Ana başlık stili */
+                .main-title {
+                    font-size: 36px; /* Yazı boyutunu büyüt */
+                    font-weight: bold; /* Kalın yazı */
+                    color: black; /* Yazı rengi siyah */
+                    text-align: center; /* Ortala */
+                    margin-top: 20px; /* Üst boşluk */
+                    margin-bottom: 20px; /* Alt boşluk */
+                }
+            </style>
+            <h1 class="main-title">2024 4.Çeyrek Yıllık Büyüme Tahmini</h1>
+            """,
+            unsafe_allow_html=True
         )
-        st.plotly_chart(fig2)
+            
 
-    else:
-        fig2.add_trace(go.Scatter(x=yıllık.index,y=yıllık["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=4),marker=dict(size=8,color="black")))
-        fig2.add_trace(go.Scatter(x=yıllık.index[:-1],y=yıllık["Büyüme"][:-1],mode='lines+markers',name="Büyüme",line=dict(color='orange', width=4),marker=dict(size=8,color="black")))
-        fig2.update_layout(
+
+        formatted_dates = pd.to_datetime(cariyıl.index).strftime('%d.%m.%Y')
+
+
+        fig3= go.Figure()
+        if dark_mode_enabled:
+            fig3.add_trace(go.Scatter(x=cariyıl.index,y=cariyıl["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=6),marker=dict(size=8,color="white")))
+            fig3.update_layout(
+                xaxis=dict(
+                    tickvals=cariyıl.index,  # Original datetime index
+                    ticktext=formatted_dates,  # Custom formatted labels
+                    tickfont=dict(size=14, family="Arial Black", color="white")
+                ),
+                yaxis=dict(
+                    tickfont=dict(size=14, family="Arial Black", color="white")
+                ),
+                font=dict(family="Arial", size=14, color="white")
+            )
+            fig3.add_trace(go.Scatter(
+                x=cariyıl.index,
+                y=cariyıl["Üst"],
+                mode='lines',
+                line=dict(color='rgba(1,1,1,1)'),
+                showlegend=False
+            ))
+            
+
+            fig3.add_trace(go.Scatter(
+                x=cariyıl.index,
+                y=cariyıl["Alt"],
+                mode='lines',
+                fill='tonexty',  # Fill between this trace and the previous one
+                fillcolor='rgba(255,165,1,0.3)',  # Semi-transparent orange color
+                line=dict(color='rgba(1,1,1,1)'),
+                name='Güven Aralığı'
+            ))
+            fig3.update_traces(line=dict(width=5)) 
+
+        else:
+            fig3.add_trace(go.Scatter(x=cariyıl.index,y=cariyıl["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=6),marker=dict(size=8,color="black")))
+            fig3.add_trace(go.Scatter(
+                x=cariyıl.index,
+                y=cariyıl["Üst"],
+                mode='lines',
+                line=dict(color='rgba(0,0,0,0)'),
+                showlegend=False
+            ))
+            
+
+            fig3.add_trace(go.Scatter(
+                x=cariyıl.index,
+                y=cariyıl["Alt"],
+                mode='lines',
+                fill='tonexty',  # Fill between this trace and the previous one
+                fillcolor='rgba(255,165,1,0.3)',  # Semi-transparent orange color
+                line=dict(color='rgba(0,0,0,0)'),
+                name='Güven Aralığı'
+            ))
+            fig3.update_traces(line=dict(width=5)) 
+            fig3.update_layout(
+                xaxis=dict(
+                    tickvals=cariyıl.index,  # Original datetime index
+                    ticktext=formatted_dates,  # Custom formatted labels
+                    tickfont=dict(size=14, family="Arial Black", color="black")
+                ),
+                yaxis=dict(
+                    tickfont=dict(size=14, family="Arial Black", color="black")
+                ),
+                font=dict(family="Arial", size=14, color="black")
+            )
+            st.plotly_chart(fig3)
+
+    
+    if tip=="Ham":
+        ham=pd.read_csv("ham.csv",index_col=0)
+        ham.index=pd.to_datetime(ham.index)
+        formatted_dates = pd.to_datetime(ham.index).strftime('%d.%m.%Y')
+        st.markdown(
+            """
+            <style>
+                /* Ana başlık stili */
+                .main-title {
+                    font-size: 36px; /* Yazı boyutunu büyüt */
+                    font-weight: bold; /* Kalın yazı */
+                    color: black; /* Yazı rengi siyah */
+                    text-align: center; /* Ortala */
+                    margin-top: 20px; /* Üst boşluk */
+                    margin-bottom: 20px; /* Alt boşluk */
+                }
+            </style>
+            <h1 class="main-title">2024 4.Çeyrek Yıllık Ham Büyüme Tahmini</h1>
+            """,
+            unsafe_allow_html=True
+        )
+        figham= go.Figure()
+
+        figham.add_trace(go.Scatter(x=ham.index,y=ham["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=6),marker=dict(size=8,color="black")))
+        figham.add_trace(go.Scatter(
+            x=ham.index,
+            y=ham["Üst"],
+            mode='lines',
+            line=dict(color='rgba(0,0,0,0)'),
+            showlegend=False
+        ))
+        
+
+        figham.add_trace(go.Scatter(
+            x=ham.index,
+            y=ham["Alt"],
+            mode='lines',
+            fill='tonexty',  # Fill between this trace and the previous one
+            fillcolor='rgba(255,165,1,0.3)',  # Semi-transparent orange color
+            line=dict(color='rgba(0,0,0,0)'),
+            name='Güven Aralığı'
+        ))
+        figham.update_traces(line=dict(width=5)) 
+        figham.update_layout(
             xaxis=dict(
-                tickvals=yıllık.index,  # Original datetime index
+                tickvals=ham.index,  # Original datetime index
                 ticktext=formatted_dates,  # Custom formatted labels
                 tickfont=dict(size=14, family="Arial Black", color="black")
             ),
@@ -380,97 +533,4 @@ if page=="Yıllık Tahmin":
             ),
             font=dict(family="Arial", size=14, color="black")
         )
-        st.plotly_chart(fig2)
-
-
-
-    st.markdown(
-        """
-        <style>
-            /* Ana başlık stili */
-            .main-title {
-                font-size: 36px; /* Yazı boyutunu büyüt */
-                font-weight: bold; /* Kalın yazı */
-                color: black; /* Yazı rengi siyah */
-                text-align: center; /* Ortala */
-                margin-top: 20px; /* Üst boşluk */
-                margin-bottom: 20px; /* Alt boşluk */
-            }
-        </style>
-        <h1 class="main-title">2024 4.Çeyrek Yıllık Büyüme Tahmini</h1>
-        """,
-        unsafe_allow_html=True
-    )
-        
-
-
-    formatted_dates = pd.to_datetime(cariyıl.index).strftime('%d.%m.%Y')
-
-
-    fig3= go.Figure()
-    if dark_mode_enabled:
-        fig3.add_trace(go.Scatter(x=cariyıl.index,y=cariyıl["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=6),marker=dict(size=8,color="white")))
-        fig3.update_layout(
-            xaxis=dict(
-                tickvals=cariyıl.index,  # Original datetime index
-                ticktext=formatted_dates,  # Custom formatted labels
-                tickfont=dict(size=14, family="Arial Black", color="white")
-            ),
-            yaxis=dict(
-                tickfont=dict(size=14, family="Arial Black", color="white")
-            ),
-            font=dict(family="Arial", size=14, color="white")
-        )
-        fig3.add_trace(go.Scatter(
-            x=cariyıl.index,
-            y=cariyıl["Üst"],
-            mode='lines',
-            line=dict(color='rgba(1,1,1,1)'),
-            showlegend=False
-        ))
-        
-
-        fig3.add_trace(go.Scatter(
-            x=cariyıl.index,
-            y=cariyıl["Alt"],
-            mode='lines',
-            fill='tonexty',  # Fill between this trace and the previous one
-            fillcolor='rgba(255,165,1,0.3)',  # Semi-transparent orange color
-            line=dict(color='rgba(1,1,1,1)'),
-            name='Güven Aralığı'
-        ))
-        fig3.update_traces(line=dict(width=5)) 
-
-    else:
-        fig3.add_trace(go.Scatter(x=cariyıl.index,y=cariyıl["Tahmin"],mode='lines+markers',name="Tahmin",line=dict(color='red', width=6),marker=dict(size=8,color="black")))
-        fig3.add_trace(go.Scatter(
-            x=cariyıl.index,
-            y=cariyıl["Üst"],
-            mode='lines',
-            line=dict(color='rgba(0,0,0,0)'),
-            showlegend=False
-        ))
-        
-
-        fig3.add_trace(go.Scatter(
-            x=cariyıl.index,
-            y=cariyıl["Alt"],
-            mode='lines',
-            fill='tonexty',  # Fill between this trace and the previous one
-            fillcolor='rgba(255,165,1,0.3)',  # Semi-transparent orange color
-            line=dict(color='rgba(0,0,0,0)'),
-            name='Güven Aralığı'
-        ))
-        fig3.update_traces(line=dict(width=5)) 
-        fig3.update_layout(
-            xaxis=dict(
-                tickvals=cariyıl.index,  # Original datetime index
-                ticktext=formatted_dates,  # Custom formatted labels
-                tickfont=dict(size=14, family="Arial Black", color="black")
-            ),
-            yaxis=dict(
-                tickfont=dict(size=14, family="Arial Black", color="black")
-            ),
-            font=dict(family="Arial", size=14, color="black")
-        )
-        st.plotly_chart(fig3)
+        st.plotly_chart(figham)
